01acf5b7ff41b0588a931accefab821b01f41b36
metha24353backhod <130365223+metha24353backhod@users.noreply.github.com>
2025-11-22 22:09:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st 
 uploaded_file = st.file_uploader("Choose a pdf file")
 key = st.text_input("enter key")
+name=st.text_input("enter csv name")
+
+
+tab1, tab2,tab3 = st.tabs(["Dashboard", "Analysis", "Raw Data"])
+
