76124382ebd20fa35cc790d3a47139afc4e70869
metha24353backhod <130365223+metha24353backhod@users.noreply.github.com>
2025-11-22 23:08:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,38 +6,39 @@ name=st.text_input("enter csv name")
 
 tab1, tab2,tab3 = st.tabs(["Dashboard", "Analysis", "Raw Data"])
 with tab1:
-  from pdf2image import convert_from_path
-  import os
+  if file_upload:
+    from pdf2image import convert_from_path
+    import os
 
-  pdf_path = file_upload
+    pdf_path = file_upload
 
-  folder_page1 = "page1"
-  folder_images = "images"
+    folder_page1 = "page1"
+    folder_images = "images"
 
-  os.makedirs(folder_page1, exist_ok=True)
-  os.makedirs(folder_images, exist_ok=True)
+    os.makedirs(folder_page1, exist_ok=True)
+    os.makedirs(folder_images, exist_ok=True)
 
-  pages = convert_from_path(pdf_path, dpi=300)
-  total = len(pages)
+    pages = convert_from_path(pdf_path, dpi=300)
+    total = len(pages)
 
-  st.write(total)
+    st.write(total)
 
-  for i, page in enumerate(pages, start=1):
+    for i, page in enumerate(pages, start=1):
 
     # ❌ Skip last page
-    if i == total:
+      if i == total:
         print(f"[SKIPPED LAST PAGE] Page {i}")
         continue
 
     # Page 1 → separate folder
-    if i == 1:
+      if i == 1:
         out = f"{folder_page1}/page_1.png"
         page.save(out, "PNG")
         print(f"[PAGE 1 SAVED] {out}")
         continue
 
     # Pages 2 to n-1 → /content/images
-    out = f"{folder_images}/page{i}.png"
-    page.save(out, "PNG")
-    print(f"[PAGE SAVED] {out}")
+      out = f"{folder_images}/page{i}.png"
+      page.save(out, "PNG")
+   
 
