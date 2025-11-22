33ec60f9f3ccd5f3b4f9c48dd7b1338a62a50449
metha24353backhod <130365223+metha24353backhod@users.noreply.github.com>
2025-11-22 22:05:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 import streamlit as st 
 st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible", width="stretch")
-key=st.text_input(label="input_name_unique", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
-uiiid=st.text_input(label="input unique value ", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch")
+key = st.text_input("enter key")
