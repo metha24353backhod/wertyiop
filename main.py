6f5290f5dc2337cff15b87e91060043e1df34b60
metha24353backhod <130365223+metha24353backhod@users.noreply.github.com>
2025-11-22 22:05:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st 
-st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible", width="stretch")
+st.file_uploader()
 key = st.text_input("enter key")
